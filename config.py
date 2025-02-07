1b877bfb2c189b4eff62043a127bc46cf642c510
antoo69 <antobarry69@gmail.com>
2025-02-07 21:39:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,10 @@ load_dotenv() if not path.exists("local.env") else load_dotenv("local.env")
 
 
 class Config:
-    api_id = int(getenv("API_ID", "0"))
-    api_hash = getenv("API_HASH", "abc")
+    api_id = int(getenv("API_ID", "23345148"))
+    api_hash = getenv("API_HASH", "fe37a47fef4345512ed47c17d3306f0b")
     bot_token = getenv("BOT_TOKEN", "123:Abc")
     log_channel = int(getenv("LOG_CHANNEL"))
-    fsub_chid = int(getenv("FORCESUB_CHANNEL"))
     db_chid = int(getenv("DB_CHANNEL"))
     blacklisted_channel = [int(x) for x in getenv("BLACKLISTED_CHANNEL", "123").split(",") if x is not None]
     channel1 = int(getenv("CHANNEL_1"))
