95eb3d4469eae3aaeb602272b600c18db82877aa
antoo69 <antobarry69@gmail.com>
2025-04-20 21:59:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,14 @@ load_dotenv() if not path.exists("local.env") else load_dotenv("local.env")
 class Config:
     api_id = int(getenv("API_ID", "23345148"))
     api_hash = getenv("API_HASH", "fe37a47fef4345512ed47c17d3306f0b")
-    bot_token = getenv("BOT_TOKEN", "123:Abc")
-    log_channel = int(getenv("LOG_CHANNEL"))
-    db_chid = int(getenv("DB_CHANNEL"))
+    bot_token = getenv("BOT_TOKEN", "8161422676:AAGnGOJ8nn-_HftdLZJ6gWqmY2xbfnJ43Bw")
+    fsub_channel = getenv("FSUB_CHANNEL", "BestieVirtual")  # TANPA awalan "@"
+    log_channel = int(getenv("LOG_CHANNEL", "-1002351111178"))
+    db_chid = int(getenv("DB_CHANNEL", "-1002351111178"))
     blacklisted_channel = [int(x) for x in getenv("BLACKLISTED_CHANNEL", "123").split(",") if x is not None]
-    channel1 = int(getenv("CHANNEL_1"))
-    channel2 = int(getenv("CHANNEL_2"))
-    channel3 = int(getenv("CHANNEL_3"))
+    channel1 = int(getenv("CHANNEL_1", "-1002280848465"))
+    channel2 = int(getenv("CHANNEL_2", "-1002079962899"))
+    channel3 = int(getenv("CHANNEL_3", "-1002348472641"))
 
 
 config = Config()
