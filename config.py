dc5f3367e92b8b1d7487dee4aa3906e99020164c
antoo69 <antobarry69@gmail.com>
2025-04-20 22:16:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ class Config:
     api_id = int(getenv("API_ID", "23345148"))
     api_hash = getenv("API_HASH", "fe37a47fef4345512ed47c17d3306f0b")
     bot_token = getenv("BOT_TOKEN", "8161422676:AAGnGOJ8nn-_HftdLZJ6gWqmY2xbfnJ43Bw")
-    fsub_channel = getenv("FSUB_CHANNEL", "BestieVirtual")  # TANPA awalan "@"
+    fsub_channel = getenv("FSUB_CHANNEL", "BestieVirtual")
+    fsub_channel2 = getenv("FSUB_CHANNEL2", "AnotherChannel")  # Channel kedua
+    fsub_channel3 = getenv("FSUB_CHANNEL3", "ThirdChannel")
     log_channel = int(getenv("LOG_CHANNEL", "-1002351111178"))
     db_chid = int(getenv("DB_CHANNEL", "-1002351111178"))
     blacklisted_channel = [int(x) for x in getenv("BLACKLISTED_CHANNEL", "123").split(",") if x is not None]
