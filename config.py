3325f042a4b6368adcae4fcd106a389bc3de65f3
antoo69 <antobarry69@gmail.com>
2026-05-03 22:43:47 +0700

    .

--- config.py
+++ config.py
@@ -21,14 +21,14 @@ def _get_int_list(name: str, default: str = "") -> list[int]:
 
 
 class Config:
-    api_id = _get_int("API_ID")
-    api_hash = getenv("API_HASH", "").strip()
-    bot_token = getenv("BOT_TOKEN", "").strip()
-    log_channel = _get_int("LOG_CHANNEL")
-    db_chid = _get_int("DB_CHANNEL")
+    api_id = _get_int("API_ID, 39539870")
+    api_hash = getenv("API_HASH", "655c51c79a9c59192dc95baaa72bb99d").strip()
+    bot_token = getenv("BOT_TOKEN", "8718028794:AAESRyihzAAqnIJaNk8eRmjg12brq9p0CaE").strip()
+    log_channel = _get_int("LOG_CHANNEL, -1002356369190")
+    db_chid = _get_int("DB_CHANNEL, -1002356369190")
     blacklisted_channel = _get_int_list("BLACKLISTED_CHANNEL")
-    owner_id = _get_int_list("OWNER_ID")
-    dev_id = _get_int_list("DEV_ID")
+    owner_id = _get_int_list("OWNER_ID, 1086812385")
+    dev_id = _get_int_list("DEV_ID, 1086812385")
     backup_dir = getenv("BACKUP_DIR", "backups")
 
     @classmethod
